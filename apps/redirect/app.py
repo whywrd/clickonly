eb3e900421726dbd64e8b0795f5d21231d2c86aa
will <wkh019@gmail.com>
2019-04-14 14:49:31 -0700

    moved passwords into env file

--- apps/redirect/app.py
+++ apps/redirect/app.py
@@ -4,12 +4,16 @@ import psycopg2
 from datetime import datetime
 import json
 from redirect_table import TO
+from os import environ
 
 """
 The redirect app.
 References inbound requests against a DB to determine the correct redirect location.
 Parameters are forwarded on redirect, however the base URL needs to be resolved.
 """
+
+REDIRECT_DB_USERNAME = environ['REDIRECT_DB_USERNAME']
+REDIRECT_DB_PASSWORD = environ['REDIRECT_DB_USERNAME']
 
 flask_app = Flask(__name__)
 celery_app = celery.Celery('app', broker='amqp://guest:guest@redirect_mq:5672')
@@ -18,7 +22,11 @@ celery_app = celery.Celery('app', broker='amqp://guest:guest@redirect_mq:5672')
 
 @celery.task
 def insert_db(location, history: list):
-    db_conn = psycopg2.connect("host=redirect_db dbname=clickonly user=clickonly password=AooQeTrYSRNpJvQ91q5LrRfD1R5OOphZ")
+    db_conn = psycopg2.connect("\
+        host=redirect_db \
+        dbname=clickonly \
+        user={user} \
+        password={password}".format(user=REDIRECT_DB_USERNAME, password=REDIRECT_DB_PASSWORD))
     cur = db_conn.cursor()
 
     cur.execute("""
